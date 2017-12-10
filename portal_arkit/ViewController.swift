77a5278e3d35aee08a9c1d2128bf18e391986a98
ebkn12 <ktennis.mqekr12@gmail.com>
2017-12-10 14:33:14 +0900

    implement to add portal

--- portal_arkit/ViewController.swift
+++ portal_arkit/ViewController.swift
@@ -20,6 +20,31 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     self.configuration.planeDetection = .horizontal
     self.sceneView.session.run(configuration)
     self.sceneView.delegate = self
+    
+    let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.handleTap(_:)))
+    self.sceneView.addGestureRecognizer(tapGestureRecognizer)
+  }
+  
+  @objc func handleTap(_ sender: UITapGestureRecognizer) {
+    guard let sceneView = sender.view as? ARSCNView else {return}
+    let touchLocation = sender.location(in: sceneView)
+    let hitTestResult = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent)
+    if !hitTestResult.isEmpty {
+      addPortal(hitTestResult: hitTestResult.first!)
+    } else {
+      
+    }
+  }
+  
+  func addPortal(hitTestResult: ARHitTestResult) {
+    let portalScene = SCNScene(named: "Portal.scnassets/Portal.scn")
+    let portalNode = portalScene?.rootNode.childNode(withName: "Portal", recursively: false)
+    let transform = hitTestResult.worldTransform
+    let planeXposition = transform.columns.3.x
+    let planeYposition = transform.columns.3.y
+    let planeZposition = transform.columns.3.z
+    portalNode?.position = SCNVector3(planeXposition, planeYposition, planeZposition)
+    self.sceneView.scene.rootNode.addChildNode(portalNode!)
   }
 
   override func didReceiveMemoryWarning() {
