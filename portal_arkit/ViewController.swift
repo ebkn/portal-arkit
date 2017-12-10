cdadd5a1112dbaa6d3d8b0962517bdecde0d0107
ebkn12 <ktennis.mqekr12@gmail.com>
2017-12-10 13:40:28 +0900

    create box

--- portal_arkit/ViewController.swift
+++ portal_arkit/ViewController.swift
@@ -9,18 +9,31 @@
 import UIKit
 import ARKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ARSCNViewDelegate {
   @IBOutlet weak var sceneView: ARSCNView!
   let configuration = ARWorldTrackingConfiguration()
-
+  @IBOutlet weak var planeDetected: UILabel!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+    self.configuration.planeDetection = .horizontal
     self.sceneView.session.run(configuration)
+    self.sceneView.delegate = self
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
+  }
+  
+  func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+    guard anchor is ARPlaneAnchor else {return}
+    DispatchQueue.main.async {
+      self.planeDetected.isHidden = false
+    }
+    DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+      self.planeDetected.isHidden = true
+    }
   }
 }
