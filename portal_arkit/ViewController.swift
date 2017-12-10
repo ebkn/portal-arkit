39e612166101aa3fe1370d46b6c4084135566943
ebkn12 <ktennis.mqekr12@gmail.com>
2017-12-10 17:00:06 +0900

    create button to change location

--- portal_arkit/ViewController.swift
+++ portal_arkit/ViewController.swift
@@ -12,7 +12,6 @@ import ARKit
 class ViewController: UIViewController, ARSCNViewDelegate {
   @IBOutlet weak var sceneView: ARSCNView!
   let configuration = ARWorldTrackingConfiguration()
-  @IBOutlet weak var planeDetected: UILabel!
   @IBOutlet weak var firstPlaceButton: UIButton!
   @IBOutlet weak var secondPlaceButton: UIButton!
   @IBOutlet weak var thirdPlaceButton: UIButton!
@@ -27,6 +26,10 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.handleTap(_:)))
     self.sceneView.addGestureRecognizer(tapGestureRecognizer)
+    
+    firstPlaceButton.isSelected = true
+    secondPlaceButton.backgroundColor = UIColor.clear
+    thirdPlaceButton.backgroundColor = UIColor.clear
   }
   
   @objc func handleTap(_ sender: UITapGestureRecognizer) {
@@ -35,8 +38,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     let hitTestResult = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent)
     if !hitTestResult.isEmpty {
       addPortal(hitTestResult: hitTestResult.first!)
-    } else {
-      
     }
   }
   
@@ -51,13 +52,13 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     self.sceneView.scene.rootNode.addChildNode(portalNode!)
     switch self.placeSelected {
     case 1:
-      addAllWalls(portalNode: portalNode!, imageFolderName: "SanFrancisco4")
+      addWalls(portalNode: portalNode!, imageFolderName: "SanFrancisco4")
     case 2:
-      addAllWalls(portalNode: portalNode!, imageFolderName: "Arekutan")
+      addWalls(portalNode: portalNode!, imageFolderName: "Areskutan")
     case 3:
-      addAllWalls(portalNode: portalNode!, imageFolderName: "Yokohama")
+      addWalls(portalNode: portalNode!, imageFolderName: "Yokohama")
     default:
-      addAllWalls(portalNode: portalNode!, imageFolderName: "SanFrancisco4")
+      addWalls(portalNode: portalNode!, imageFolderName: "SanFrancisco4")
     }
   }
 
@@ -69,38 +70,57 @@ class ViewController: UIViewController, ARSCNViewDelegate {
   func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
     guard anchor is ARPlaneAnchor else {return}
     DispatchQueue.main.async {
-      self.planeDetected.isHidden = false
+      print("portal has not been visible")
     }
     DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
-      self.planeDetected.isHidden = true
+      print("portal becomes visible")
     }
   }
   
-  func addAllWalls(portalNode: SCNNode, imageFolderName: String) {
-    self.addWalls(nodeName: "roof", portalNode: portalNode, imageName: "\(imageFolderName)/posy")
-    self.addWalls(nodeName: "floor", portalNode: portalNode, imageName: "\(imageFolderName)/negy")
-    self.addWalls(nodeName: "backWall", portalNode: portalNode, imageName: "\(imageFolderName)/posz")
-    self.addWalls(nodeName: "sidePaneA", portalNode: portalNode, imageName: "\(imageFolderName)/posx")
-    self.addWalls(nodeName: "sidePaneB", portalNode: portalNode, imageName: "\(imageFolderName)/negx")
-    self.addWalls(nodeName: "sideDoorA", portalNode: portalNode, imageName: "\(imageFolderName)/negz")
-    self.addWalls(nodeName: "sideDoorB", portalNode: portalNode, imageName: "\(imageFolderName)/negz")
+  func addWalls(portalNode: SCNNode, imageFolderName: String) {
+    self.addWall(nodeName: "roof", portalNode: portalNode, imageName: "\(imageFolderName)/posy")
+    self.addWall(nodeName: "floor", portalNode: portalNode, imageName: "\(imageFolderName)/negy")
+    self.addWall(nodeName: "backWall", portalNode: portalNode, imageName: "\(imageFolderName)/posz")
+    self.addWall(nodeName: "sideWallA", portalNode: portalNode, imageName: "\(imageFolderName)/posx")
+    self.addWall(nodeName: "sideWallB", portalNode: portalNode, imageName: "\(imageFolderName)/negx")
+    self.addWall(nodeName: "sideDoorA", portalNode: portalNode, imageName: "\(imageFolderName)/negz")
+    self.addWall(nodeName: "sideDoorB", portalNode: portalNode, imageName: "\(imageFolderName)/negz")
   }
   
-  func addWalls(nodeName: String, portalNode: SCNNode, imageName: String) {
+  func addWall(nodeName: String, portalNode: SCNNode, imageName: String) {
     let child = portalNode.childNode(withName: nodeName, recursively: true)
     child?.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "Portal.scnassets/\(imageName).jpg")
+    child?.renderingOrder = 200
+    if let mask = child?.childNode(withName: "mask", recursively: false) {
+      mask.geometry?.firstMaterial?.transparency = 0.000001
+    }
   }
   
   
   @IBAction func firstPlaceButtonTapped(_ sender: Any) {
     self.placeSelected = 1
+    firstPlaceButton.isSelected = true
+    secondPlaceButton.isSelected = false
+    thirdPlaceButton.isSelected = false
+    secondPlaceButton.backgroundColor = UIColor.clear
+    thirdPlaceButton.backgroundColor = UIColor.clear
   }
   
   @IBAction func secondPlaceButtonTapped(_ sender: Any) {
     self.placeSelected = 2
+    firstPlaceButton.isSelected = false
+    secondPlaceButton.isSelected = true
+    thirdPlaceButton.isSelected = false
+    firstPlaceButton.backgroundColor = UIColor.clear
+    thirdPlaceButton.backgroundColor = UIColor.clear
   }
   
   @IBAction func thirdPlaceButtonTapped(_ sender: Any) {
     self.placeSelected = 3
+    firstPlaceButton.isSelected = false
+    secondPlaceButton.isSelected = false
+    thirdPlaceButton.isSelected = true
+    firstPlaceButton.backgroundColor = UIColor.clear
+    secondPlaceButton.backgroundColor = UIColor.clear
   }
 }
