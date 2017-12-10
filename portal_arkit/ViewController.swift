5b6198563a9541e5ec325165253c7f134c585a79
ebkn12 <ktennis.mqekr12@gmail.com>
2017-12-10 16:09:11 +0900

    import images and create mask

--- portal_arkit/ViewController.swift
+++ portal_arkit/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController, ARSCNViewDelegate {
   @IBOutlet weak var sceneView: ARSCNView!
   let configuration = ARWorldTrackingConfiguration()
   @IBOutlet weak var planeDetected: UILabel!
+  @IBOutlet weak var firstPlaceButton: UIButton!
+  @IBOutlet weak var secondPlaceButton: UIButton!
+  @IBOutlet weak var thirdPlaceButton: UIButton!
+  var placeSelected: Int = 1
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -45,6 +49,16 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     let planeZposition = transform.columns.3.z
     portalNode?.position = SCNVector3(planeXposition, planeYposition, planeZposition)
     self.sceneView.scene.rootNode.addChildNode(portalNode!)
+    switch self.placeSelected {
+    case 1:
+      addAllWalls(portalNode: portalNode!, imageFolderName: "SanFrancisco4")
+    case 2:
+      addAllWalls(portalNode: portalNode!, imageFolderName: "Arekutan")
+    case 3:
+      addAllWalls(portalNode: portalNode!, imageFolderName: "Yokohama")
+    default:
+      addAllWalls(portalNode: portalNode!, imageFolderName: "SanFrancisco4")
+    }
   }
 
   override func didReceiveMemoryWarning() {
@@ -60,5 +74,33 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
       self.planeDetected.isHidden = true
     }
+  }
+  
+  func addAllWalls(portalNode: SCNNode, imageFolderName: String) {
+    self.addWalls(nodeName: "roof", portalNode: portalNode, imageName: "\(imageFolderName)/posy")
+    self.addWalls(nodeName: "floor", portalNode: portalNode, imageName: "\(imageFolderName)/negy")
+    self.addWalls(nodeName: "backWall", portalNode: portalNode, imageName: "\(imageFolderName)/posz")
+    self.addWalls(nodeName: "sidePaneA", portalNode: portalNode, imageName: "\(imageFolderName)/posx")
+    self.addWalls(nodeName: "sidePaneB", portalNode: portalNode, imageName: "\(imageFolderName)/negx")
+    self.addWalls(nodeName: "sideDoorA", portalNode: portalNode, imageName: "\(imageFolderName)/negz")
+    self.addWalls(nodeName: "sideDoorB", portalNode: portalNode, imageName: "\(imageFolderName)/negz")
+  }
+  
+  func addWalls(nodeName: String, portalNode: SCNNode, imageName: String) {
+    let child = portalNode.childNode(withName: nodeName, recursively: true)
+    child?.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "Portal.scnassets/\(imageName).jpg")
+  }
+  
+  
+  @IBAction func firstPlaceButtonTapped(_ sender: Any) {
+    self.placeSelected = 1
+  }
+  
+  @IBAction func secondPlaceButtonTapped(_ sender: Any) {
+    self.placeSelected = 2
+  }
+  
+  @IBAction func thirdPlaceButtonTapped(_ sender: Any) {
+    self.placeSelected = 3
   }
 }
